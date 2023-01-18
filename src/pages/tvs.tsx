244726e0019deb60836656b773077bec69844367
Rus <rus.sharafiev@gmail.com>
2023-01-19 01:33:31 +0300

    Added: load next page on scroll

--- src/pages/tvs.tsx
+++ src/pages/tvs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../store/hooks'
 import { fetchPopularTvs, fetchTopRatedTvs, fetchAiringTodayTvs, ptNext, trtNext, attNext } from '../store/tvsSlice'
@@ -10,28 +10,67 @@ import CircularProgressIndicator from '../ui/cpi'
 const Tvs: React.FC = () => {
     const tvs = useAppSelector((state: RootState) => state.tvs)
     const dispatch = useAppDispatch()
+    const endOfPage = useRef(null)
     let { list } = useParams()
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }, [tvs.popular.firstLoadDone, tvs.top_rated.firstLoadDone, tvs.airing_today.firstLoadDone])
 
     useEffect(() => {
         switch (list) {
             case 'popular':
-                if (tvs.popular.status === 'idle') dispatch(fetchPopularTvs(tvs.popular.page));
-                break;
+                if (tvs.popular.status === 'idle') dispatch(fetchPopularTvs(tvs.popular.page))
+                break
             case 'top_rated':
-                if (tvs.top_rated.status === 'idle') dispatch(fetchTopRatedTvs(tvs.top_rated.page));
-                break;
+                if (tvs.top_rated.status === 'idle') dispatch(fetchTopRatedTvs(tvs.top_rated.page))
+                break
             case 'airing_today':
-                if (tvs.airing_today.status === 'idle') dispatch(fetchAiringTodayTvs(tvs.airing_today.page));
-                break;
+                if (tvs.airing_today.status === 'idle') dispatch(fetchAiringTodayTvs(tvs.airing_today.page))
+                break
         }
-    }, [list, tvs.popular.page])
+    }, [list])
+
+    useEffect(() => {
+
+        if (list && tvs[list].status === 'complete') {
+
+            const intersectionObserver = new IntersectionObserver((entries) => {
+                if (entries[0].intersectionRatio <= 0) return
+
+                switch (list) {
+                    case 'popular':
+                        dispatch(fetchPopularTvs(tvs.popular.page + 1))
+                            .then(() => console.log(`Has been loaded page ${tvs.popular.page + 1}`))
+                        dispatch(ptNext())
+                        break
+                    case 'top_rated':
+                        dispatch(fetchTopRatedTvs(tvs.top_rated.page + 1))
+                            .then(() => console.log(`Has been loaded page ${tvs.top_rated.page + 1}`))
+                        dispatch(trtNext())
+                        break
+                    case 'airing_today':
+                        dispatch(fetchAiringTodayTvs(tvs.airing_today.page + 1))
+                            .then(() => console.log(`Has been loaded page ${tvs.airing_today.page + 1}`))
+                        dispatch(attNext())
+                        break
+                }
+            });
+
+            endOfPage.current && intersectionObserver.observe(endOfPage.current);
+            return () => {
+                endOfPage.current && intersectionObserver.unobserve(endOfPage.current);
+            }
+        }
+
+    }, [endOfPage, list, tvs.popular.status, tvs.top_rated.status, tvs.airing_today.status])
 
     if (!list) return null
 
     return (
         <>
-            <main className={tvs[list].status !== 'complete' ? 'cards hidden' : 'cards'}>
-                {tvs[list].status === 'complete' && tvs[list].content.map((tv: Tv) =>
+            <main className={tvs[list].firstLoadDone ? 'cards' : 'cards hidden'}>
+                {tvs[list].content.map((tv: Tv) =>
                     <Card key={tv.id}
                         img={tv.poster_path}
                         title={tv.name}
@@ -39,9 +78,10 @@ const Tvs: React.FC = () => {
                         rating={tv.vote_average}
                         votes={tv.vote_count} />
                 )}
-                <button type='button' onClick={() => dispatch(ptNext())} >Next</button>
+                <div className='cards-loader' ref={endOfPage}></div>
             </main>
-            {tvs[list].status === 'loading' && <CircularProgressIndicator className='cpi' />}
+            <div className={tvs[list].status === 'loading' ? 'page-is-loading show' : 'page-is-loading'}>Стр {tvs[list].page}</div>
+            {!tvs[list].firstLoadDone && tvs[list].status === 'loading' && <CircularProgressIndicator className='cpi' />}
         </>
     )
 };
