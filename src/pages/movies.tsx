3d308f940a1e725639247c42951e3d26a232d1f9
Rus <rus.sharafiev@gmail.com>
2023-01-25 02:03:02 +0300

    Change: Observer Remove: Card component

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../store/hooks'
 import { fetchPopularMovies, fetchTopRatedMovies, fetchUpcomingMovies, pmNext, trmNext, umNext } from '../store/moviesSlice'
 import { RootState } from '../store/store'
-import { Movie } from '../types/cards'
-import Card, { CardSkeleton } from '../ui/card'
+import { MovieCard } from '../types/cards'
+import { MediaCardSkeleton } from '../ui/skeletons'
+import Rating from '../ui/rating'
 
 const Movies: React.FC = () => {
     const movies = useAppSelector((state: RootState) => state.movies)
@@ -34,54 +35,54 @@ const Movies: React.FC = () => {
     }, [list])
 
     useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].intersectionRatio <= 0) return
 
-        if (list && movies[list].status === 'complete') {
-
-            const intersectionObserver = new IntersectionObserver((entries) => {
-                if (entries[0].intersectionRatio <= 0) return
-
-
+            if (list && movies[list].status === 'complete') {
                 switch (list) {
                     case 'popular':
-                        dispatch(fetchPopularMovies(movies.popular.page + 1))
-                            .then(() => console.log(`Has been loaded page ${movies.popular.page + 1}`))
                         dispatch(pmNext())
+                        dispatch(fetchPopularMovies(movies.popular.page + 1))
+                            .then(() => console.log(`Has been loaded page ${movies.popular.page}`))
                         break
                     case 'top_rated':
+                        dispatch(trmNext())
                         dispatch(fetchTopRatedMovies(movies.top_rated.page + 1))
                             .then(() => console.log(`Has been loaded page ${movies.top_rated.page + 1}`))
-                        dispatch(trmNext())
                         break
                     case 'upcoming':
+                        dispatch(umNext())
                         dispatch(fetchUpcomingMovies(movies.upcoming.page + 1))
                             .then(() => console.log(`Has been loaded page ${movies.upcoming.page + 1}`))
-                        dispatch(umNext())
                         break
                 }
-            });
-
-            endOfPage.current && intersectionObserver.observe(endOfPage.current);
-            return () => {
-                endOfPage.current && intersectionObserver.unobserve(endOfPage.current);
             }
+        });
+
+        if (endOfPage.current) observer.observe(endOfPage.current);
+
+        return () => {
+            if (endOfPage.current) observer.unobserve(endOfPage.current);
         }
 
-    }, [endOfPage, list, movies.popular.status, movies.top_rated.status, movies.upcoming.status])
+    }, [endOfPage, movies.popular.status, movies.top_rated.status, movies.upcoming.status])
 
     if (!list) return null
 
     return (
         <>
             <div className={'cards'}>
-                {movies[list].content.map((movie: Movie) =>
-                    <Card key={movie.id}
-                        img={movie.poster_path}
-                        title={movie.title}
-                        originalTitle={movie.original_title}
-                        rating={movie.vote_average}
-                        votes={movie.vote_count} />
+                {movies[list].content.map((movie: MovieCard) =>
+                    <Link to={`/movie/${movie.id}`} className='card' key={movie.id}>
+                        <img src={movie.poster_path} />
+                        <Rating radius={22.5} rating={parseFloat(movie.vote_average ? movie.vote_average.toFixed(1) : '0')} votes={movie.vote_count} />
+                        <div className='title'>
+                            <span>{movie.title}</span>
+                            <span>{movie.original_title}</span>
+                        </div>
+                    </Link>
                 )}
-                {movies[list].status === 'loading' && [...Array(20)].map((e, i) => <CardSkeleton key={`skeleton-${i}`} />)}
+                {movies[list].status === 'loading' && [...Array(20)].map((e, i) => <MediaCardSkeleton key={`skeleton-${i}`} />)}
                 <div className='cards-loader' ref={endOfPage}></div>
             </div>
         </>
