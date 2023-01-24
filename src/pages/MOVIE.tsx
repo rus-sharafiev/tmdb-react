67b9fb4c3a7a34110e5270b1a51b821af17656a1
Rus <rus.sharafiev@gmail.com>
2023-01-25 02:01:56 +0300

    Add: Movie page

--- src/pages/MOVIE.tsx
+++ src/pages/MOVIE.tsx
@@ -1,9 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import proxyImageLoader from "../helpers/proxyImageLoader"
+import { Movie } from "../types/movie"
+
+const preloadMovie = async (content: Movie) => {
+    content.backdrop_path = await proxyImageLoader(content.backdrop_path, 'w1280')
+    content.poster_path = await proxyImageLoader(content.poster_path, 'w500')
+    return content;
+}
 
 const Movie: React.FC = () => {
+    let { id } = useParams()
+    const [movie, setMovie] = useState<Movie>()
+
+    useEffect(() => {
+        fetch(`/api/movie/${id}`)
+            .then(res => res.json())
+            .then(obj => preloadMovie(obj))
+            .then(movie => setMovie(movie))
+    }, [id])
+
+    useEffect(() => {
+        movie && console.log(movie)
+    }, [movie])
+
+    if (!movie) return null
+
     return (
         <main className="movie">
-
+            <img src={movie.poster_path} alt='poster' />
+            <img src={movie.backdrop_path} alt='backdrop' />
         </main>
     )
 }
---
+++ src/types/movie.ts
@@ -0,0 +1,88 @@
+interface Collection {
+    id: number,
+    name: string,
+    poster_path: string,
+    backdrop_path: string
+}
+
+interface Genre {
+    id: number,
+    name: string
+}
+
+interface ProductionCompanie {
+    id: number,
+    logo_path: string,
+    name: string,
+    origin_country: string
+}
+
+interface ProductionCountrie {
+    iso_3166_1: string,
+    name: string
+}
+
+interface SpokenLanguage {
+    english_name: string,
+    iso_639_1: string,
+    name: string
+}
+
+interface Video {
+    iso_639_1: string,
+    iso_3166_1: string,
+    name: string,
+    key: string,
+    site: string,
+    size: string,
+    type: string,
+    official: boolean,
+    published_at: string,
+    id: string
+}
+
+interface Image {
+    aspect_ratio: number,
+    height: number,
+    iso_639_1: string,
+    file_path: string,
+    vote_average: number,
+    vote_count: number,
+    width: number
+}
+
+export interface Movie {
+    adult: boolean,
+    backdrop_path: string,
+    belongs_to_collection: Collection | null,
+    budget: number,
+    genres: Genre[],
+    homepage: string,
+    id: number,
+    imdb_id: string,
+    original_language: string,
+    original_title: string,
+    overview: string
+    popularity: number,
+    poster_path: string,
+    production_companies: ProductionCompanie[],
+    production_countries: ProductionCountrie[],
+    release_date: string,
+    revenue: number,
+    runtime: number,
+    spoken_languages: SpokenLanguage[],
+    status: string,
+    tagline: string,
+    title: string,
+    video: boolean,
+    vote_average: number,
+    vote_count: number,
+    videos: {
+        results: Video[]
+    },
+    images: {
+        backdrops: Image[],
+        logos: Image[],
+        posters: Image[]
+    }
+}
