d4a69f4a579515df672a0c5cb2247a53bb474f30
Rus <rus.sharafiev@gmail.com>
2023-02-01 01:41:18 +0300

    Declare: collection interface

---
+++ src/types/collection.ts
@@ -0,0 +1,25 @@
+export interface Part {
+    adult: boolean,
+    backdrop_path: string,
+    genre_ids: number[],
+    id: number,
+    original_language: string,
+    original_title: string,
+    overview: string,
+    popularity: number,
+    poster_path: string,
+    release_date: string,
+    title: string,
+    video: boolean,
+    vote_average: number,
+    vote_count: number
+}
+
+export interface Collection {
+    id: number,
+    name: string,
+    overview: string,
+    poster_path: string,
+    backdrop_path: string,
+    parts: Part[]
+}
