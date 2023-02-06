e550f16ebe2c60920427815737c14b948b8a44e1
Rus <rus.sharafiev@gmail.com>
2023-02-07 02:59:33 +0300

    Add: Custom Image proxy

--- src/helpers/proxyImageLoader.ts
+++ src/helpers/proxyImageLoader.ts
@@ -5,20 +5,23 @@
  * @returns proxyed and loaded image URL
  */
 
-const proxyImageLoader = async (path: string, size: string, fallBack?: string): Promise<string> => {
+const proxyImageLoader = async (path: string, size: string, fallBack?: string, oneMoreAttempt?: boolean): Promise<string> => {
 
     if (!path) return fallBack ?? ''
 
     return new Promise((res) => {
-        let url = 'https://image.tmdb.org/t/p/' + size + path
+        let url = 'https://image.rutmdb.ru/t/p/' + size + path
         let image = new Image()
-        image.crossOrigin = "Anonymous"
-        image.src = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=' + encodeURIComponent(url)
+        image.src = url
         image.onload = () => {
             res(image.src)
         }
         image.onerror = () => {
-            res('/img/no_image.png')
+            if (!oneMoreAttempt)
+                setTimeout(() => {
+                    console.log('One more attemt to load image')
+                    proxyImageLoader(path, size, fallBack, true)
+                }, 2000);
         }
 
     });
