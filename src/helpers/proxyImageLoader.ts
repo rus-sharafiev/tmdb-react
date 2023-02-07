2e6cb93eabd98cc8447ecb3d27e83e543caabc8c
Rus <rus.sharafiev@gmail.com>
2023-02-08 02:34:46 +0300

    Add: TMDB image sizes info

--- src/helpers/proxyImageLoader.ts
+++ src/helpers/proxyImageLoader.ts
@@ -1,10 +1,48 @@
+interface ImageSizes {
+    backdrop_sizes: [
+        "w300",
+        "w780",
+        "w1280",
+        "original"
+    ],
+    logo_sizes: [
+        "w45",
+        "w92",
+        "w154",
+        "w185",
+        "w300",
+        "w500",
+        "original"
+    ],
+    poster_sizes: [
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original"
+    ],
+    profile_sizes: [
+        "w45",
+        "w185",
+        "h632",
+        "original"
+    ],
+    still_sizes: [
+        "w92",
+        "w185",
+        "w300",
+        "original"
+    ]
+}
+
 /**
  * @param path Specifies image
  * @param size You can get from TMDB API
  * @param fallBack Can be specified if no image is provided
  * @returns proxyed and loaded image URL
  */
-
 const proxyImageLoader = async (path: string, size: string, fallBack?: string, oneMoreAttempt?: boolean): Promise<string> => {
 
     if (!path) return fallBack ?? ''
