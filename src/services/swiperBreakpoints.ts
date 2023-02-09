ba458e5709d58983446f66851e561269c9f3525c
Rus <rus.sharafiev@gmail.com>
2023-02-09 20:57:40 +0300

    Add: Swiper breakpoints

---
+++ src/services/swiperBreakpoints.ts
@@ -0,0 +1,67 @@
+// Collection swiper breakpoints
+export const collectionSwiperBreakpoints = {
+    820: {
+        slidesPerView: 2
+    },
+    978: {
+        slidesPerView: 2
+    },
+    1136: {
+        slidesPerView: 3
+    },
+    1452: {
+        slidesPerView: 4
+    },
+    1610: {
+        slidesPerView: 5
+    },
+    1768: {
+        slidesPerView: 6
+    },
+    1926: {
+        slidesPerView: 7
+    },
+    2084: {
+        slidesPerView: 8
+    },
+    2242: {
+        slidesPerView: 9
+    }
+}
+
+// Actors swiper breakpoints
+export const ActorsSwiperBreakpoints = {
+    820: {
+        slidesPerView: 2
+    },
+    948: {
+        slidesPerView: 2
+    },
+    1076: {
+        slidesPerView: 2
+    },
+    1204: {
+        slidesPerView: 3
+    },
+    1332: {
+        slidesPerView: 4
+    },
+    1460: {
+        slidesPerView: 5
+    },
+    1588: {
+        slidesPerView: 6
+    },
+    1716: {
+        slidesPerView: 7
+    },
+    1844: {
+        slidesPerView: 8
+    },
+    1972: {
+        slidesPerView: 9
+    },
+    2100: {
+        slidesPerView: 10
+    }
+}
