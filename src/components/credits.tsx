0c7bf5a508549383d1c75e3c82cdf0181abdb77d
Rus <rus.sharafiev@gmail.com>
2023-02-12 22:35:13 +0300

    Add: 'writer' or 'screenplay'

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { Navigation } from "swiper"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { preloadCast } from "../services/preloaders"
-import { ActorsSwiperBreakpoints } from "../services/swiperBreakpoints"
+import { actorsSwiperBreakpoints } from "../services/swiperBreakpoints"
 import { Actor, Credits } from "../types/movie"
 
 const Credits: React.FC<{ data: Credits }> = ({ data }) => {
@@ -27,7 +27,7 @@ const Credits: React.FC<{ data: Credits }> = ({ data }) => {
                 <div className="screenplay">
                     <span>Сценарий</span>
                     <span>{data.crew.map(p =>
-                        p.job === 'Screenplay' &&
+                        (p.job === 'Writer' || p.job === 'Screenplay') &&
                         <Link to={`/person/${p.id}`} key={'crew-' + p.id}>{p.name}</Link>)}</span>
                 </div>
                 <div className="producer">
@@ -37,11 +37,12 @@ const Credits: React.FC<{ data: Credits }> = ({ data }) => {
                         <Link to={`/person/${p.id}`} key={'crew-' + p.id}>{p.name}</Link>)}</span>
                 </div>
             </div>
-            {actors?.length > 0 &&
+            {actors.length > 0
+                ?
                 <div className="cast">
                     <button type="button" className='cast-prev-btn material-symbols-rounded unselectable'>navigate_before</button>
                     <Swiper
-                        breakpoints={ActorsSwiperBreakpoints}
+                        breakpoints={actorsSwiperBreakpoints}
                         modules={[Navigation]}
                         navigation={{
                             prevEl: '.cast-prev-btn',
@@ -49,7 +50,7 @@ const Credits: React.FC<{ data: Credits }> = ({ data }) => {
                         }}
                     >
                         {actors.map((actor: Actor) =>
-                            <SwiperSlide key={'part-' + actor.id}>
+                            <SwiperSlide key={'actor-' + actor.id}>
                                 <Link to={`/person/${actor.id}`} className='card unselectable' >
                                     <img src={actor.profile_path} alt='photo' />
                                     <div className='name'>
@@ -62,6 +63,7 @@ const Credits: React.FC<{ data: Credits }> = ({ data }) => {
                     </Swiper>
                     <button type="button" className='cast-next-btn material-symbols-rounded unselectable'>navigate_next</button>
                 </div>
+                : <div className="cast" style={{ opacity: '0' }} />
             }
         </>
     )
