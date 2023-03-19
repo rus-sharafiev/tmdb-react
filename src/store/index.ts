efaf5630732ecf186ee9e01694e1800a743d79d2
Rus <rus.sharafiev@gmail.com>
2023-03-19 21:49:41 +0300

    Refactor: Extract middleware to constraint

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
-import rootReducer from './reducers'
-
-import { cardsApi } from '../services/api/cardsApi'
-import { collectionApi } from '../services/api/collectionApi'
-import { seasonApi } from '../services/api/seasonApi'
-import { mediaApi } from '../services/api/mediaApi'
-import { recommendationsApi } from '../services/api/recommendationsApi'
-import { creditsApi } from '../services/api/creditsApi'
-import { contentApi } from '../services/api/contentApi'
+import { rootReducer, middlewareArray } from './reducers'
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false
-        })
-            .concat(cardsApi.middleware)
-            .concat(collectionApi.middleware)
-            .concat(seasonApi.middleware)
-            .concat(mediaApi.middleware)
-            .concat(recommendationsApi.middleware)
-            .concat(creditsApi.middleware)
-            .concat(contentApi.middleware)
+    middleware: (gDM) =>
+        gDM({ serializableCheck: false }).concat(middlewareArray)
 })
 
 setupListeners(store.dispatch)
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit"
 
 import pagesReducer from './pagesSlice'
+import searchResultsReducer from './searchResultsSlice'
 import { cardsApi } from '../../services/api/cardsApi'
 import { collectionApi } from '../../services/api/collectionApi'
 import { seasonApi } from '../../services/api/seasonApi'
@@ -8,9 +9,11 @@ import { mediaApi } from '../../services/api/mediaApi'
 import { recommendationsApi } from '../../services/api/recommendationsApi'
 import { creditsApi } from '../../services/api/creditsApi'
 import { contentApi } from '../../services/api/contentApi'
+import { searchApi } from "../../services/api/searchApi"
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
     pages: pagesReducer,
+    searchResults: searchResultsReducer,
 
     [cardsApi.reducerPath]: cardsApi.reducer,
     [collectionApi.reducerPath]: collectionApi.reducer,
@@ -19,7 +22,16 @@ const rootReducer = combineReducers({
     [recommendationsApi.reducerPath]: recommendationsApi.reducer,
     [creditsApi.reducerPath]: creditsApi.reducer,
     [contentApi.reducerPath]: contentApi.reducer,
-
+    [searchApi.reducerPath]: searchApi.reducer,
 })
 
-export default rootReducer
+export const middlewareArray = [
+    cardsApi.middleware,
+    collectionApi.middleware,
+    seasonApi.middleware,
+    mediaApi.middleware,
+    recommendationsApi.middleware,
+    creditsApi.middleware,
+    contentApi.middleware,
+    searchApi.middleware
+]
